3926fcbec5328100d01ee56b257acfa3a5445198
BlackFox <1916840725@qq.com>
2017-01-16 10:55:49 +0800

    Application全局变量的修改

--- app/src/main/java/ucai/cn/fulicenter/application/FuLiCenterApplication.java
+++ app/src/main/java/ucai/cn/fulicenter/application/FuLiCenterApplication.java
@@ -1,22 +1,28 @@
 package ucai.cn.fulicenter.application;
 
 import android.app.Application;
+import android.content.Context;
+
+import ucai.cn.fulicenter.controller.activity.MainActivity;
 
 /**
  * Created by BlackFox on 2017/1/10.
  */
-public class FuLiCenterApplication extends Application {
-    private static FuLiCenterApplication instance=null;
+public  class FuLiCenterApplication extends Application {
+    private static Context instance;
 
     public FuLiCenterApplication() {
 
     }
 
-    public FuLiCenterApplication getInstance() {
-        if (instance == null) {
-            instance = new FuLiCenterApplication();
-        }
-        return this.instance;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance=getApplicationContext();
+    }
+
+    public static Context getInstance() {
+        return instance;
 
     }
 
